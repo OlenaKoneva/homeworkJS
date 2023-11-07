915c0465b6797aa5fc04ba805493ce0bdf114d17
lena <olena.koneva@ezlo.com>
2023-11-07 14:20:52 +0000

    result_hw3

--- lesson3hw.js
+++ lesson3hw.js
@@ -8,3 +8,34 @@ function drawTriangle(high, symbol) {
 }
 
 drawTriangle(5, '=')
+
+function drawTriangleV2(high, symbol) {
+  let stringToOut = '';
+  let i = 0;
+  while ( i < high ) {
+    stringToOut += symbol;
+    console.log(stringToOut);
+    i++;
+  }
+}
+
+drawTriangleV2(6, '0')
+
+//Завдання 2
+let result = 0;
+for (let i = 1; i <= 100; i++) {
+  result += i;
+  if (result % 3) {
+    console.log(result);
+  }
+}
+
+//Завдання3
+function pow(x,y){
+  let result = x;
+  for (i = 1; i < y; i++ ){
+    result *=x;
+    console.log(result)
+  }
+}
+pow(5,5)
