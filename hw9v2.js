3d88da8a71e2ce9f57ac6400fc6dcfcc27e18520
lena <olena.koneva@ezlo.com>
2023-11-21 21:08:47 +0000

    result_hw9v2

---
+++ hw9v2.js
@@ -0,0 +1,53 @@
+const company = {
+  name: 'Велика Компанія',
+  type:'Головна компанія',
+  platform: 'Платформа для продажу квитків',
+  sellsSolution: 'Рішення для продажу квитків',
+  clients: [
+    {
+      name: 'Клієнт 1',
+      type: 'subCompany',
+      uses: 'ПО для продажу квитків',
+      sells: 'Рішення для продажу квитків',
+      partners: [
+        {
+          name: 'Клієнт 1.1',
+          type: 'subSubCompany',
+          uses: 'Рішення для продажу квитків',
+          sells: 'Рішення для продажу квитків',
+        },
+        {
+          name: 'Клієнт 1.2',
+          type: 'subSubCompany',
+          uses: 'Рішення для продажу квитків',
+          sells: 'Рішення для продажу квитків',
+          partners: [
+            {
+              name: 'Клієнт 1.2.3',
+              type: 'subSubCompany',
+              uses: 'Рішення для продажу квитків',
+              sells: 'Рішення для продажу квитків',
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: 'Клієнт 2',
+      type: 'subCompany',
+      uses: 'ПО для продажу квитків',
+      sells: 'Рішення для продажу квитків'
+    }
+  ]
+};
+
+function findValueByKey(companyName){
+   let filteredCompany = company.clients.filter((item) => {
+     return item.name === companyName;
+   });
+
+  console.log(filteredCompany);
+
+}
+
+findValueByKey('Клієнт 1');
